5bd781befbf25223a28547799472346dd2dc48fe
Resilliance <Jkesguerra2050@yahoo.com>
2022-01-15 00:20:58 -0500

    Initialized project and cocoapods

---
+++ RecycPal/RecycPal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecycPal
+//
+//  Created by Justin Esguerra on 1/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
