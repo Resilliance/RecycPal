169e7abc10d2d8efe721660b9446460025bd54fe
Resi <resi@Resis-MBP.lan>
2023-08-21 18:27:35 -0400

    [ADD] Update lol, I can't figure out a good commit msg on the fly

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivy.uix.label import Label
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.behaviors import ButtonBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.bottomnavigation import MDBottomNavigationItem
 # from kivymd.uix.toolbar import MDTopAppBar
 from kivymd.uix.label import MDLabel
 from kivymd.uix.button import MDFillRoundFlatButton,MDFloatingActionButton,MDRoundFlatButton
@@ -17,9 +19,12 @@ from kivy.uix.image import Image
 from kivy.uix.scrollview import ScrollView
 from kivymd.uix.card import MDCard
 from kivy.graphics.texture import Texture
+# from kivy_garden.mapview import MapView, MapMarkerPopup
+from plyer import gps, utils
 import json
 import time
 import os
+import datetime
 
 # Libraries AI
 import cv2
@@ -49,6 +54,9 @@ class RecycpalClickableLabel(ButtonBehavior, MDLabel):
     """
     pass
 
+class RecycpalMapView(MDScreen):
+    pass
+
 class RecycpalHistoryDetailView(MDScreen):
     directory = None
     
@@ -57,6 +65,14 @@ class RecycpalHistoryDetailView(MDScreen):
 
 
     def on_enter(self):
+
+        # Load in JSON to access the data
+        existing_data = RecycpalApp.get_running_app().load_image_paths()
+        
+        # Get Directory Index
+        dir_index = RecycpalApp.get_running_app().captured_images.index(self.directory)
+
+
         detail_box = self.ids.detail_box
         card = MDCard(size_hint=(0.9,0.9),
                       md_bg_color=(0.894,0.890,0.667,1),
@@ -72,14 +88,21 @@ class RecycpalHistoryDetailView(MDScreen):
                            size_hint=(0.9,0.9),
                            pos_hint={'center_x':0.5,'center_y':0.5})
         
-        card_name = MDLabel(text=f"Name: ",size_hint_y=None, height="40dp",font_style="H5")
-        card_date = MDLabel(text=f"Date Taken: ",size_hint_y=None, height="40dp",font_style="H5")
-        card_objects_detected = MDLabel(text=f"Objects Detected: ",size_hint_y=None, height="40dp",font_style="H5")
+        # Pull Existing Data into MDLabels
+        card_name = MDLabel(text=f"Name: {existing_data['name'][f'{dir_index}']}",size_hint_y=None, height="40dp",font_style="H5")
+        card_date = MDLabel(text=f"Date Taken: {existing_data['datetime'][f'{dir_index}']}",size_hint_y=None, height="40dp",font_style="H5")
+        card_objects_detected = MDLabel(text=f"Objects Detected: {', '.join(set(existing_data['objects_detected'][f'{dir_index}']))}",size_hint_y=None, height="40dp",font_style="H5")
+        
+        # Add widgets into the card_box 
         card_box.add_widget(card_image)
         card_box.add_widget(card_name)
         card_box.add_widget(card_date)
         card_box.add_widget(card_objects_detected)
+
+        # Add card_box into card widget
         card.add_widget(card_box)
+
+        # Add card into the detail_box (MDBoxLayout)
         detail_box.add_widget(card)
 
 class RecycpalHistory(MDScreen):
@@ -108,6 +131,13 @@ class RecycpalCamera(MDScreen):
     def __init__(self, **kwargs):
         super(RecycpalCamera, self).__init__(**kwargs)
         self.widgets = {}
+        self.image_name = ""
+        # self.detections = None
+
+    # def on_enter(self):
+    #     super().on_enter()
+        # self.ids.camera.index = 1
+
 
     def capture(self):
         camera = self.ids['camera']
@@ -121,6 +151,8 @@ class RecycpalCamera(MDScreen):
         else:
             # On desktop platforms, you could use the current directory or any directory you have permissions for
             directory = "data/images/IMG_{}.png".format(time_start)
+        
+        self.image_name = "IMG_{}.png".format(time_start)
         camera.export_to_png(directory)
         image = cv2.imread(directory)
         results = RecycpalApp.model(directory)
@@ -177,7 +209,7 @@ class RecycpalCamera(MDScreen):
                         text_color=(1,1,1,1),
                         size_hint=(0.3, 0.05),
                         pos_hint={'center_x': 0.3,'center_y': 0.15},
-                        on_release=lambda x: self.save_image(directory),
+                        on_release=lambda x: self.save_image(directory,detections),
                     )
 
         discard_button = MDFillRoundFlatButton(
@@ -202,15 +234,22 @@ class RecycpalCamera(MDScreen):
         self.ids['camera_screen'].add_widget(save_button)
         self.ids['camera_screen'].add_widget(discard_button)
         
-    def save_image(self, directory):
+    def save_image(self, directory,detections):
         print('save\n',directory)
         # Add image directory to the captured_images array
         RecycpalApp.get_running_app().captured_images.append(directory)
+
+        # Get Directory Index
+        dir_index = RecycpalApp.get_running_app().captured_images.index(directory)
+
         # Load existing data
         existing_data = RecycpalApp.get_running_app().load_image_paths()
 
         # Update directory data
         existing_data['directory'] = RecycpalApp.get_running_app().captured_images
+        existing_data['name'][f"{dir_index}"] = self.image_name
+        existing_data['datetime'][f"{dir_index}"] = datetime.datetime.now().strftime('%Y-%m-%d')
+        existing_data['objects_detected'][f"{dir_index}"] = [detection['name'] for detection in detections]
 
         with open('image_paths.json','w') as file:
             json.dump(existing_data,file)
@@ -243,16 +282,113 @@ class RecycpalCamera(MDScreen):
         self.ids['camera_box'].add_widget(self.ids['camera'])
         self.ids['camera_screen'].add_widget(self.ids['capture_button'])
 
+class RecycpalInfoDetailView(MDScreen):
+    material = None
+
+    def set_material(self, material):
+        self.material = material
+
+    def on_enter(self):
+        # Load in JSON to access the data
+        existing_data = RecycpalApp.get_running_app().load_static_data()
+        
+        # Get Use the Material to pull the correct information
+        material_info = existing_data[f"{self.material}"]
+
+
+        material_box = self.ids.material_box
+        card = MDCard(size_hint=(0.9,0.9),
+                        md_bg_color=(0.894,0.890,0.667,1),
+                        pos_hint={'center_x':0.5,'center_y':0.5})
+        
+        card_box = MDBoxLayout(orientation='vertical',
+                                md_bg_color=(0.894,0.890,0.667,1),
+                                radius=[15,],
+                                padding="10dp")
+        card_box_header = MDBoxLayout(orientation='horizontal',
+                                      md_bg_color=(0.894,0.890,0.667,1),
+                                      size_hint= (1,0.2))
+        card_box_header_name = MDLabel(text=f"{self.material}",
+                                       font_style="H3",
+                                       text_color=(1,1,1,1),
+                                       pos_hint={'center_x': 0.7,'center_y': 0.5})
+
+        card_box_header_image = Image(source=f"staticfiles/images/{self.material.lower()}.png",
+                                      allow_stretch=True,
+                                      size_hint=(0.7,0.7),
+                                      pos_hint={'center_x':0.6,'center_y':0.5})
+
+        card_box_body = MDGridLayout(cols=1,
+                            padding="10dp",
+                            size_hint_y=1.5,
+                            md_bg_color=(0.894,0.890,0.667,1))
+
+        # Pull Existing Data into MDLabels
+        card_info = MDLabel(text=f"{material_info}",
+                            size_hint_y=None,
+                            height="40dp",
+                            font_style="H5")
+        
+        # Add widgets into the card_box_header 
+        card_box_header.add_widget(card_box_header_image)
+        card_box_header.add_widget(card_box_header_name)
+        card_box.add_widget(card_box_header)
+
+        # Add info widget to card_box_body
+        card_box_body.add_widget(card_info)
+        card_box.add_widget(card_box_body)
+
+        # Add card_box into card widget
+        card.add_widget(card_box)
+
+        # Add card into the detail_box (MDBoxLayout)
+        material_box.add_widget(card)
+
+
 class RecycpalInfo(MDScreen):
     """
     NOTE: Reminder to eventually make the Cards in the info screen
     dynamically populate instead of statically
     """
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.is_intialized = False
+
+    def on_enter(self):
+        
+        if self.is_intialized == False:
+            # Get Static Data
+            existing_data = RecycpalApp.get_running_app().load_static_data()
+
+            # Create Card for each Material
+            for key in existing_data:
+                card = MDCard(size_hint=(1,None),
+                            md_bg_color=(0.894,0.890,0.667,1),
+                            on_release=lambda x, key=key: RecycpalApp.get_running_app().open_info_card(key))
+                card_box = MDBoxLayout(orientation='horizontal')
+                card_image = Image(source=f"staticfiles/images/{key.lower()}.png",
+                                size_hint=(0.4,0.85),
+                                pos_hint={'center_x':0.5,'center_y':0.5})
+                card_label = MDLabel(markup=True,
+                                    text=f"{key}",
+                                    size_hint=(0.8,1),
+                                    pos_hint= {'center_x':0.6},
+                                    text_color= (0,0,0,1),
+                                    font_style="H3")
+                
+                card_box.add_widget(card_image)
+                card_box.add_widget(card_label)
+                card.add_widget(card_box)
+                self.ids.info_box.add_widget(card)
+
+                self.is_intialized = True
+        else:
+            pass
+
 
 class RecycpalApp(MDApp):       
-    # model = YOLO('yolov8n.pt')
     model = YOLO('yolov8n-seg.pt')
+
     # Call previous images taken
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -262,16 +398,29 @@ class RecycpalApp(MDApp):
         self.theme_cls.material_style = "M3"
         self.theme_cls.theme_style = "Dark"
 
+        # Request Android Permissions
+        self.request_android_permissions()
+
         # Defining the Screens
         self.history = RecycpalHistory(name="History")
         self.camera = RecycpalCamera(name="Camera")
         self.info = RecycpalInfo(name="Info")
+        # self.maps = RecycpalMapView(name="Maps")
         self.card_detail = RecycpalHistoryDetailView(name="Detail")
+        self.info_detail = RecycpalInfoDetailView(name="Material")
 
         self.root.ids.history.add_widget(self.history)
         self.root.ids.history.add_widget(self.card_detail)
         self.root.ids.camera.add_widget(self.camera)
+        # self.root.ids.info.add_widget(self.maps)
         self.root.ids.info.add_widget(self.info)
+        self.root.ids.info.add_widget(self.info_detail)
+
+    def request_android_permissions(self):
+        if platform == 'android':
+            # Request permissions for Android
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.ACCESS_FINE_LOCATION])
 
     
     def load_image_paths(self):
@@ -280,9 +429,18 @@ class RecycpalApp(MDApp):
                 return json.load(file)
         except FileNotFoundError:
             return {"directory": []}
+        
+    def load_static_data(self):
+        try:
+            with open('static_data.json','r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {"directory": []}
+        
     def open_card(self,directory):
         print("\n\n",directory)
         self.root.ids.history.get_screen('Detail').set_directory(directory)
+        self.root.ids.history.get_screen('History').ids['history_screen'].scroll_y = 1
         self.root.ids.history.transition = MDSlideTransition(direction="left")
         self.root.ids.history.current = "Detail"
         
@@ -290,12 +448,22 @@ class RecycpalApp(MDApp):
         self.root.ids.history.get_screen('Detail').ids['detail_box'].clear_widgets()
         self.root.ids.history.get_screen('Detail').ids['detail_screen'].scroll_y = 1
         self.root.ids.history.transition = MDSlideTransition(direction="right")
-        self.root.ids.history.get_screen('History').on_enter()
+        # self.root.ids.history.get_screen('History').on_enter()
         self.root.ids.history.current = "History"
 
     def back_to_info(self):
-        # self.root.ids.info.get_screen('Paper')
-        pass
+        self.root.ids.info.get_screen('Material').ids['material_box'].clear_widgets()
+        self.root.ids.info.get_screen('Material').ids['material_scroll'].scroll_y = 1
+        self.root.ids.info.transition = MDSlideTransition(direction="right")
+        self.root.ids.info.current = "Info"
+        # pass
+
+    def open_info_card(self,material):
+        print("\n\n",material)
+        self.root.ids.info.get_screen("Material").set_material(material)
+        # self.root.ids.info.get_screen("Info").ids['info_box'].clear_widgets()
+        self.root.ids.info.transition = MDSlideTransition(direction="left")
+        self.root.ids.info.current = "Material"
 
 
 RecycpalApp().run()
